3d1f7d6df02a60515bdeb4faeb0f7fb25f94a656
Sumeet416 <sumeet.17758@sakec.ac.in>
2025-05-16 22:26:20 +0530

    task done

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,4 +2,24 @@ const config = {
   plugins: ["@tailwindcss/postcss"],
 };
 
-export default config;
+export default {
+
+  content: [
+    './templates/**/*.{html,js}',
+    './static/**/*.{js,html}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        bevietnam: ['"Be Vietnam Pro"', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+        nunito: ['Nunito', 'sans-serif'],
+        robotoslab: ['"Roboto Slab"', 'serif'],
+        sourcesans: ['"Source Sans 3"', 'sans-serif'],
+        ubuntu: ['Ubuntu', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+
+};
